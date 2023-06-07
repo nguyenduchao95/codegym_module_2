633e15aed390e2090457f6bb500f5e6c7a90caf9
Hao <nguyenduchao95@gmail.com>
2023-06-07 16:56:25 +0700

    Xây dựng lớp Fan

---
+++ section4/src/b4/Test.java
@@ -0,0 +1,10 @@
+package b4;
+
+public class Test {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(Fan.FAST, true, 10, "yellow");
+        Fan fan2 = new Fan(Fan.MEDIUM, false, 5, "blue");
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
+    }
+}
