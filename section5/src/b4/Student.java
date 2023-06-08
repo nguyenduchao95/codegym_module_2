91ab959c22cf36e80c0a96b97a0825a0efdba37d
Hao <nguyenduchao95@gmail.com>
2023-06-08 14:31:27 +0700

    Xây dựng lớp chỉ ghi trong Java

---
+++ section5/src/b4/Student.java
@@ -0,0 +1,13 @@
+package b4;
+
+public class Student {
+    private String name = "john", classes = "C02";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
